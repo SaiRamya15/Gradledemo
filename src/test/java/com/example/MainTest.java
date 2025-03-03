ccf0245d938c04a0b2a45e798bffa385814ce0d3
SaiRamya15 <sairamya5169@gmail.com>
2025-03-03 16:51:05 +0530

    Create MainTest.java

---
+++ src/test/java/com/example/MainTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class MainTest {
+    @Test
+    public void testMain() {
+        assertEquals(2, 1 + 1); // Simple test
+    }
+}
